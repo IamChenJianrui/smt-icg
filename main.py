4db637de24ae78a170987ab0bd29b007b8f6e5d8
kaidong <kaidong.chen@kaidong.chen>
2021-02-20 18:19:58 +0800

    函数入口

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ if __name__ == '__main__':
     start = time.time()
     domain = PDDLParser("./pddl/Take_Away.pddl")
     gen = FormulaGenerator(domain)
-    arr = gen.generate()
-    formula = gen.build_formula(arr)
+    formula = gen.generate()
     cost = time.time() - start
 
-    # if not os.path.exists("./log"):
-    #     os.mkdir("./log")
-    # with open("./log/%s" % domain.name, "a") as f:
-    #     print("\n*******************Finished*******************")
-    #     print(arr)
-    #     print('N-formula:\n\t %s' % formula)
-    #     print('Total time cost:\n\t %s' % cost)
-    #     print('N-formula: %s' % formula, file=f)
-    #     print('Total time cost: %s' % cost, file=f)
+    if not os.path.exists("./log"):
+        os.mkdir("./log")
+    with open("./log/%s" % domain.name, "a") as f:
+        print("\n*******************Finished*******************")
+        print('N-formula:\n\t %s' % formula)
+        print('Total time cost:\n\t %s' % cost)
+        print('N-formula: %s' % formula, file=f)
+        print('Total time cost: %s' % cost, file=f)
